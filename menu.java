1c791931f973b7d288f90dd6adc29af9f61ec850
24Victor <borenvictorzheng@iesmontsia.org>
2023-02-22 13:49:19 +0100

    gf

--- menu.java
+++ menu.java
@@ -1,11 +1,4 @@
-char menu (String[] opcions){
+import java.util.Scanner;
+public class menu{
 
-    /*Imprimir un menu */
-    imprimirArray(opcions);
-
-    /*Guardar opció que escull usuari */
-    char op = llegirUnChar();
-
-    /*Torna un resultat l'eleccio de l'usuari */
-    return op;
 }
