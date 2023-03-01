007977b701d677605b63c638703f7b1b78592894
aleix2001 <aleixenrich123@gmail.com>
2023-03-01 14:21:43 +0100

    Funcio recuperar

---
+++ Aleix/menuProvaAleix.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class menuProvaAleix {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        /* Test prova menu */
+        /* Array Ple */
+        //String[] myArray = {"dilluns", "dimarts", "dimecres","dijous","divendres"};
+        //int numElem = 5;
+
+
+        /* Array Buit */
+        //String[] myArray = {"", "", "","",""};
+        //int numElem = 0;
+
+
+        /* Array a mig omplir */
+        String[] myArray = {"dilluns", "dimarts", "","",""};
+
+
+        System.out.print("Buscar element posicio:");
+        
+
+        /*Scanner sc1 = new Scanner(System.in);
+        System.out.print("escollir Llista:");
+        String llista = sc.nextLine(); */
+        int posicio;
+        posicio = llegirEnter ("Introdueix la posició que vols consultar");
+
+        String prova;
+        prova = recuperar(posicio, myArray );
+        System.out.println(prova);
+
+
+    }
+
+    /**
+     * troba l'element x que està a la posició p
+    Entrada: posició p, llista
+    Sortida: element x
+
+     */
+    public static String recuperar ( int p, String[] llista) {
+        
+        return llista[p];
+    }
+
+    /*Demanar enter al usuari i el torna */
+    public static int llegirEnter (String msg){
+        System.out.println(msg);
+        int enter = sc.nextInt();
+        return enter;
+
+    }
+
+}
