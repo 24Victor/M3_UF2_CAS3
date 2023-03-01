f157c85742193237d1fc4eb1682d6e97f3725c27
raulcasanovagisbert <raulcasanova@iesmontsia.org>
2023-03-01 14:26:00 +0100

    14:26

--- Raul/inserir.java
+++ Raul/inserir.java
@@ -1,15 +1,49 @@
-public class inserir {
+package Raul;
 
-    public static void inserirElement (int [] myArray){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("INTRODUEIX LA POSICIO ON VOLS INTRODUIR L'ELEMENT");
-        int p = sc.nextInt(); 
-        int c = 0;
-        for(int i = p; i>c ; i--){
-            myArray [i] = myArray [i-1];
+import java.util.Scanner;
+
+public class inserir {
+    private static Scanner sc = new Scanner(System.in);
+    private static int numElem = 10;
+    private static String[] myArray = new String[numElem];
+    private static int p = 0;
+
+    public static void main(String[] args) {
+        // Titol del programa
+        System.out.println(" INSERIR ");
+
+        // Demanem a l'usuari que introdueixi un número
+        System.out.println(" INGRESA LA POSICIO ON VOLS INSERIR L'ELEMENT: ");
+        int m = sc.nextInt();
+
+        // Condicional (Si el número es menor a 0 o mayor a 10 demanarem otro cop el número)
+        while (m < 0 || m >= numElem) {
+            System.out.println("ERROR, TORNA A INTRODUIR UN NUMERO DEL 0 AL " + (numElem - 1));
+            m = sc.nextInt();
         }
-        c++;
-        System.out.println("INTRODUEIX EL NOU ELEMENT");
-        myArray[p] = sc.nextInt();
+
+        // Demanem a l'usuari que ingresi un element a l'array
+        System.out.println(" INGRESA L'ELEMENT QUE VOLS AFEGIR: ");
+        String x = sc.next();
+
+        // Condicional (si el número d'elements es == a la llargada de l'array s'imprimeix un missatge)
+        if (p == myArray.length) {
+            System.out.println(" L'ARRAY ESTÀ PLENA. NO ES POT INSERIR MÉS ELEMENTS. ");
+        } else {
+            // Desplaça els elements cap a la dreta per a fer espai per a el nou element
+            for (int i = numElem - 1; i >= m; i--) {
+                myArray[i] = myArray[i - 1];
+            }
+
+            // Inserta el nou element a la posició especificada
+            myArray[m] = x;
+            p++;
+
+            // Imprimim l'array per a fer la comprovació de que tot funciona correctament
+            System.out.println(" ELEMENTS RESTANTS DE LA LLISTA: ");
+            for (int i = 0; i < p; i++) {
+                System.out.println(myArray[i]);
+            }
+        }
     }
 }
---
+++ Raul/suprimir.java
@@ -0,0 +1,40 @@
+package Raul;
+
+
+public class suprimir {
+    
+    private static void suprimir(String[] myArray) {
+        /* suprimir: elimina de la llista l'element de la posició p
+            Entrada: posició p, llista
+            Sortida: llista*/
+                
+        /* Suprimir dada */
+                
+        /* Titol del programa */
+        System.out.println("Suprimir");
+                                
+        /* Demanem a l'usuari quina posició vol eliminar */
+        System.out.println("Ingresa la posició de l'element que vols eliminar: ");
+        int position = sc.nextInt();
+
+        /* Desplacem tots els elements detràs de la posició especificada cap a l'esquerra */
+        for (int i = position; i < numElem - 1; i++) {
+            myArray[i] = myArray[i + 1];
+        }
+        /* Verifiquem que la posició ingresada sigui valida */
+        while(p > position || position > numElem){
+            System.out.println("Error, tornar a introduir un numero del 1 al 10.");
+            position = sc.nextInt();
+        }
+
+        /* Restem una posició a l'array */
+        numElem--;
+
+        /* Imprimim l'array ja actualitzada */
+        System.out.println("Elements restants de la llista: ");
+        for (int i = 0; i < numElem; i++) {
+            System.out.println(myArray[i]);
+        }
+    }
+    
+}
