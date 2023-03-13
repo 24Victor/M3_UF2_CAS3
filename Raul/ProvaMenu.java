a7a955b42595114b6549424c89583a553d5653f4
raulcasanovagisbert <raulcasanova@iesmontsia.org>
2023-03-13 21:08:01 +0100

    dilluns 13

--- Raul/ProvaMenu.java
+++ Raul/ProvaMenu.java
@@ -1,42 +1,43 @@
+package Raul;
+
 import java.util.Scanner;
 
 public class ProvaMenu {
     public static void main(String[] args) {
-        /*test prova menu */
+        /* test prova menu */
 
-    String[] prova = {"dilluns (L)", "dimarts (M)", "dimecres (X)"};
-    imprimirArray(prova);
+        String[] prova = {"dilluns (L)", "dimarts (M)", "dimecres (X)"};
+        imprimirArray(prova);
     }
 
     public static void imprimirArray(String[] a) {
-        for(int i=0; i<a.length; i++){
+        for(int i=0; i<a.length; i++) {
             System.out.println(a[i]);
-
         }
     }
 
+    /*
+    public static char menu(String[] opcions) {
+        // imprimir un menu
+        imprimirArray(opcions);
+
+        // guardar opcio que escull usuari
+        Scanner teclat = new Scanner(System.in);
+        char op = teclat.next().charAt(0);
+
+        // tornar opció escollida
+        return op;
+    }
+    */
+
+    /*
+    // Llegeix una paraula del teclat i retorna el primer caracter de la paraula
+    public static char llegirUnChar() {
+        Scanner teclat = new Scanner(System.in);
+        String llegida = teclat.next();
+        System.out.println("Debug, paraula llegida " + llegida);
+        char caracter = llegida.charAt(0);
+        return caracter;
+    }
+    */
 }
-
-    /*resultat = menu(prova);*/
-
-    //System.out.println("debug, veure que val el resultat" + resultat);
-
-/*public static char menu (String[] opcions) {
-    imprimir un menu 
-    imprimirArray(opcions);
-
-    guardar opcio que escull usuari 
-    char op = llegirUnChar();
-
-    tornar opció escollida 
-    return 'c';
-    
-}*/
-
-/*Llegeix una paraula del teclat i retorna
- * el primer caracter de la paraula
-public static char llegirUnChar(){
-    Scanner teclat = new Scanner(System.in);
-    String llegida = teclat new Scanner(System.in);
-    System.out.println("Debug, paraula llegida" + llegida);
-    char caracter = llegida.charAt(0);*/
--- Raul/suprimir.java
+++ Raul/suprimir.java
@@ -5,29 +5,39 @@ import java.util.Scanner;
 public class suprimir {
 
     private static Scanner sc = new Scanner(System.in);
-    private static int numElem = 10;    
-    private static void suprimir(String[] myArray) {
-        /* suprimir: elimina de la llista l'element de la posició p
-            Entrada: posició p, llista
-            Sortida: llista*/
+    private static int numElem = 10;
 
-        /* Suprimir dada */
-                
+    public static void main(String[] args) {
         /* Titol del programa */
         System.out.println("Suprimir");
-                                
+
+        /* Demanem a l'usuari que introdueixi la llista */
+        String[] myArray = new String[numElem];
+        for (int i = 0; i < numElem; i++) {
+            System.out.println("Ingresa l'element " + (i + 1) + ": ");
+            myArray[i] = sc.next();
+        }
+
         /* Demanem a l'usuari quina posició vol eliminar */
         System.out.println("Ingresa la posició de l'element que vols eliminar: ");
         int position = sc.nextInt();
 
-        /* Desplacem tots els elements detràs de la posició especificada cap a l'esquerra */
-        for (int i = position; i < numElem - 1; i++) {
-            myArray[i] = myArray[i + 1];
-        }
         /* Verifiquem que la posició ingresada sigui valida */
-        while(p > position || position > numElem){
-            System.out.println("Error, tornar a introduir un numero del 1 al 10.");
+        while (position < 1 || position > numElem) {
+            System.out.println("Error, torna a introduir un numero del 1 al 10.");
             position = sc.nextInt();
+        }
+
+        /* Suprimir dada */
+        suprimir(myArray, position);
+    }
+
+    public static void suprimir(String[] myArray, int position) {
+        /* Suprimir dada */
+
+        /* Desplacem tots els elements detràs de la posició especificada cap a l'esquerra */
+        for (int i = position - 1; i < numElem - 1; i++) {
+            myArray[i] = myArray[i + 1];
         }
 
         /* Restem una posició a l'array */
@@ -39,5 +49,5 @@ public class suprimir {
             System.out.println(myArray[i]);
         }
     }
-    
+
 }
